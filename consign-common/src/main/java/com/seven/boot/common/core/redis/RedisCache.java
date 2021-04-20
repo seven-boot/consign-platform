3af5b4a8580623339c09d5660ade510e26407031
seven <1042899106@qq.com>
2021-04-20 17:15:25 +0800

    增加配置读取类，redis工具类

---
+++ consign-common/src/main/java/com/seven/boot/common/core/redis/RedisCache.java
@@ -0,0 +1,47 @@
+package com.seven.boot.common.core.redis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * spring redis 工具类
+ *
+ * @author QH
+ */
+@Component
+public class RedisCache {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    /**
+     * 缓存基本的对象，Integer、String、实体类等
+     *
+     * @param key 缓存的键值
+     * @param value 缓存的值
+     */
+    public <T> void setCacheObject(final String key, final T value) {
+        redisTemplate.opsForValue().set(key, value);
+    }
+
+    /**
+     * 缓存基本的对象，Integer、String、实体类等
+     *
+     * @param key 缓存的键值
+     * @param value 缓存的值
+     * @param timeout 时间
+     * @param timeUnit 时间颗粒度
+     */
+    public <T> void setCacheObject(final String key, final T value, final Integer timeout, final TimeUnit timeUnit) {
+        redisTemplate.opsForValue().set(key, value, timeout, timeUnit);
+    }
+
+    public <T> T getCacheObject(final String key) {
+        ValueOperations<String, T> operations = redisTemplate.opsForValue();
+        return operations.get(key);
+    }
+}
