0897f9ca2f6e3fae98144d9a3b4f0d976c72a50d
seven <1042899106@qq.com>
2021-04-20 18:51:40 +0800

    增加spring security认证

---
+++ consign-core/src/main/java/com/seven/boot/core/security/filter/JwtAuthenticationTokenFilter.java
@@ -0,0 +1,25 @@
+package com.seven.boot.core.security.filter;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * token 过滤器 验证 token 有效性
+ *
+ * @author QH
+ */
+@Component
+public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // TODO 验证token
+        filterChain.doFilter(request, response);
+    }
+}
