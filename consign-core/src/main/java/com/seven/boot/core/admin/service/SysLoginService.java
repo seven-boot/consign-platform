da1e321031e06953b5e3f6c297d22b25ad9dc9f0
seven <1042899106@qq.com>
2021-04-20 23:11:21 +0800

    登录、token、跨域

---
+++ consign-core/src/main/java/com/seven/boot/core/admin/service/SysLoginService.java
@@ -0,0 +1,66 @@
+package com.seven.boot.core.admin.service;
+
+import com.seven.boot.common.constant.Constants;
+import com.seven.boot.common.core.domain.model.LoginUser;
+import com.seven.boot.common.core.redis.RedisCache;
+import com.seven.boot.common.exception.CustomException;
+import com.seven.boot.common.exception.user.CaptchaException;
+import com.seven.boot.common.exception.user.CaptchaExpireException;
+import com.seven.boot.common.exception.user.UserPasswordNotMatchException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+/**
+ * 登录验证方法
+ *
+ * @author QH
+ */
+@Component
+public class SysLoginService {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Resource
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private RedisCache redisCache;
+
+    public String login(String userName, String password, String code, String uuid) {
+        String verifyKey = Constants.CAPTCHA_CODE_KEY + uuid;
+        String captcha = redisCache.getCacheObject(verifyKey);
+        // TODO 是否删除 verifyKey
+        if (captcha == null) {
+            throw new CaptchaExpireException();
+        }
+        if (!code.equalsIgnoreCase(captcha)) {
+
+            throw new CaptchaException();
+        }
+        // 用户验证
+        Authentication authentication = null;
+        try {
+            // 该方法会去调用UserDetailsServiceImpl.loadUserByUsername
+            authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userName, password));
+        } catch (Exception e) {
+            if (e instanceof BadCredentialsException) {
+                // TODO 记录日志
+                throw new UserPasswordNotMatchException();
+            } else {
+                // TODO 记录日志
+                throw new CustomException(e.getMessage());
+            }
+        }
+
+        LoginUser loginUser = (LoginUser) authentication.getPrincipal();
+        // 生成token
+        return tokenService.createToken(loginUser);
+    }
+}
---
+++ consign-core/src/main/java/com/seven/boot/core/admin/service/TokenService.java
@@ -0,0 +1,107 @@
+package com.seven.boot.core.admin.service;
+
+import com.seven.boot.common.constant.Constants;
+import com.seven.boot.common.core.domain.model.LoginUser;
+import com.seven.boot.common.core.redis.RedisCache;
+import com.seven.boot.common.util.ServletUtils;
+import com.seven.boot.common.util.ip.AddressUtils;
+import com.seven.boot.common.util.ip.IpUtils;
+import eu.bitwalker.useragentutils.UserAgent;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * token 验证处理
+ *
+ * @author QH
+ */
+@Component
+public class TokenService {
+
+    // 令牌自定义标识
+    @Value("${token.header}")
+    private String header;
+
+    // 令牌秘钥
+    @Value("${token.secret}")
+    private String secret;
+
+    // 令牌有效期（默认30分钟）
+    @Value("${token.expireTime}")
+    private int expireTime;
+
+    protected static final long MILLIS_SECOND = 1000;
+
+    protected static final long MILLIS_MINUTE = 60 * MILLIS_SECOND;
+
+    private static final Long MILLIS_MINUTE_TEN = 20 * 60 * 1000L;
+
+    @Autowired
+    private RedisCache redisCache;
+
+    public String createToken(LoginUser loginUser) {
+        String token = UUID.randomUUID().toString();
+        loginUser.setToken(token);
+        setUserAgent(loginUser);
+        refreshToken(loginUser);
+
+        Map<String, Object> claims = new HashMap<>();
+        claims.put(Constants.LOGIN_USER_KEY, token);
+        return createToken(claims);
+    }
+
+    public void refreshToken(LoginUser loginUser) {
+        loginUser.setLoginTime(System.currentTimeMillis());
+        loginUser.setExpireTime(loginUser.getLoginTime() + expireTime * MILLIS_MINUTE);
+        // 根据uuid将loginUser缓存
+        String tokenKey = getTokenKey(loginUser.getToken());
+        redisCache.setCacheObject(tokenKey, loginUser, expireTime, TimeUnit.MINUTES);
+    }
+
+    public void setUserAgent(LoginUser loginUser) {
+        UserAgent userAgent = UserAgent.parseUserAgentString(ServletUtils.getRequest().getHeader("User-Agent"));
+        String ip = IpUtils.getIpAddr(ServletUtils.getRequest());
+        loginUser.setIpaddr(ip);
+        loginUser.setLoginLocation(AddressUtils.getRealAddressByIP(ip));
+        loginUser.setBrowser(userAgent.getBrowser().getName());
+        loginUser.setOs(userAgent.getOperatingSystem().getName());
+    }
+
+    /**
+     * 从数据声明生成令牌
+     *
+     * @param claims 数据声明
+     * @return 令牌
+     */
+    private String createToken(Map<String, Object> claims) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .signWith(SignatureAlgorithm.HS512, secret).compact();
+    }
+
+    /**
+     * 从令牌中获取数据声明
+     *
+     * @param token 令牌
+     * @return 数据声明
+     */
+    private Claims parseToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(secret)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private String getTokenKey(String uuid) {
+        return Constants.LOGIN_TOKEN_KEY + uuid;
+    }
+}
