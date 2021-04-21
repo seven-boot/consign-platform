275357d6d0c7fd2f514c3c5bf9e615ad5188c523
seven <1042899106@qq.com>
2021-04-21 11:14:54 +0800

    提交验证码接口

---
+++ consign-admin-api/src/main/java/com/seven/boot/admin/controller/common/CaptchaController.java
@@ -0,0 +1,77 @@
+package com.seven.boot.admin.controller.common;
+
+import com.google.code.kaptcha.Producer;
+import com.seven.boot.common.constant.Constants;
+import com.seven.boot.common.core.domain.R;
+import com.seven.boot.common.core.redis.RedisCache;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.FastByteArrayOutputStream;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Base64;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 验证码操作处理
+ *
+ * @author QH
+ */
+@RestController
+public class CaptchaController {
+
+    @Resource(name = "captchaProducer")
+    private Producer captchaProducer;
+    @Resource(name = "captchaProducerMath")
+    private Producer captchaProducerMath;
+
+    @Autowired
+    private RedisCache redisCache;
+
+    /** 验证码类型 */
+    @Value("${consign.captchaType}")
+    private String captchaType;
+
+    @GetMapping("captcha-image")
+    @ApiOperation("生成验证码")
+    public R getCaptchaCode(HttpServletResponse response) throws IOException {
+        // 保存验证码信息
+        String uuid = UUID.randomUUID().toString();
+        String verifyKey = Constants.CAPTCHA_CODE_KEY + uuid;
+
+        String capStr = null, code = null;
+        BufferedImage image = null;
+
+        if ("math".equals(captchaType)) {
+            String capText = captchaProducerMath.createText();
+            capStr = capText.substring(0, capText.lastIndexOf("@"));
+            code = capText.substring(capText.lastIndexOf("@") + 1);
+            image = captchaProducerMath.createImage(capStr);
+        } else if ("char".equals(captchaType)) {
+            capStr = code = captchaProducer.createText();
+            image = captchaProducer.createImage(capStr);
+        }
+
+        redisCache.setCacheObject(verifyKey, code, Constants.CAPTCHA_EXPIRATION, TimeUnit.MINUTES);
+        // 转换流信息写出
+        FastByteArrayOutputStream os = new FastByteArrayOutputStream();
+        try {
+            ImageIO.write(image, "jpg", os);
+        } catch (IOException e) {
+            return R.error(e.getMessage());
+        }
+
+        R r = R.success();
+        r.put("uuid", uuid);
+        r.put("img", Base64.getEncoder().encode(os.toByteArray()));
+        return r;
+    }
+}
