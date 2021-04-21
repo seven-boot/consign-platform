6b310fee369e4a3804448bbaf7172695ff4cb294
seven <1042899106@qq.com>
2021-04-21 23:22:26 +0800

    用户业务处理

---
+++ consign-dao/src/main/java/com/seven/boot/system/mapper/SysUserMapper.java
@@ -0,0 +1,19 @@
+package com.seven.boot.system.mapper;
+
+import com.seven.boot.common.core.domain.entity.SysUser;
+
+/**
+ * 用户表 数据层
+ *
+ * @author QH
+ */
+public interface SysUserMapper {
+
+    /**
+     * 通过用户名查询用户
+     *
+     * @param userName 用户名
+     * @return 用户对象信息
+     */
+    SysUser selectUserByUserName(String userName);
+}
---
+++ consign-dao/src/main/java/com/seven/boot/system/service/impl/SysUserServiceImpl.java
@@ -0,0 +1,30 @@
+package com.seven.boot.system.service.impl;
+
+import com.seven.boot.common.core.domain.entity.SysUser;
+import com.seven.boot.system.mapper.SysUserMapper;
+import com.seven.boot.system.service.ISysUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * 用户 业务层处理
+ *
+ * @author QH
+ */
+@Service
+public class SysUserServiceImpl implements ISysUserService {
+
+    @Autowired
+    private SysUserMapper userMapper;
+
+    /**
+     * 通过用户名查询用户
+     *
+     * @param userName 用户名
+     * @return 用户对象信息
+     */
+    @Override
+    public SysUser selectUserByUserName(String userName) {
+        return userMapper.selectUserByUserName(userName);
+    }
+}
