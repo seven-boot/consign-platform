3425fee35690112cee0fc69ab3ba0128f9a38373
seven <1042899106@qq.com>
2021-04-22 19:31:34 +0800

    增加菜单接口

--- consign-admin-api/src/main/java/com/seven/boot/admin/controller/system/SysLoginController.java
+++ consign-admin-api/src/main/java/com/seven/boot/admin/controller/system/SysLoginController.java
@@ -3,10 +3,12 @@ package com.seven.boot.admin.controller.system;
 import com.seven.boot.common.annotation.CurrentUser;
 import com.seven.boot.common.constant.Constants;
 import com.seven.boot.common.core.domain.R;
+import com.seven.boot.common.core.domain.entity.SysMenu;
 import com.seven.boot.common.core.domain.entity.SysUser;
 import com.seven.boot.common.core.domain.model.LoginBody;
 import com.seven.boot.core.admin.service.SysLoginService;
 import com.seven.boot.core.admin.service.TokenService;
+import com.seven.boot.system.service.ISysMenuService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author QH
@@ -26,7 +30,7 @@ public class SysLoginController {
     private SysLoginService loginService;
 
     @Autowired
-    private TokenService tokenService;
+    ISysMenuService menuService;
 
     @PostMapping("login")
     @ApiOperation("登录")
@@ -49,6 +53,13 @@ public class SysLoginController {
         r.put("user", user);
 
         return r;
+    }
+
+    @GetMapping("get-routers")
+    @ApiOperation("获取路由信息")
+    public R getRouters(@CurrentUser SysUser user) {
+        List<SysMenu> menus = menuService.selectMenuTreeByUserId(user.getUserId());
+        return R.success(menuService.buildMenus(menus));
     }
 
 }
