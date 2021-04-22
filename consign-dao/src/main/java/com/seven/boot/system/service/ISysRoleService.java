5737a9ac6e6542aedd4b00428246954174607018
seven <1042899106@qq.com>
2021-04-22 19:31:14 +0800

    菜单信息查询

---
+++ consign-dao/src/main/java/com/seven/boot/system/service/ISysRoleService.java
@@ -0,0 +1,9 @@
+package com.seven.boot.system.service;
+
+/**
+ * 角色业务层
+ *
+ * @author QH
+ */
+public interface ISysRoleService {
+}
---
+++ consign-dao/src/main/java/com/seven/boot/system/service/impl/SysRoleServiceImpl.java
@@ -0,0 +1,13 @@
+package com.seven.boot.system.service.impl;
+
+import com.seven.boot.system.service.ISysRoleService;
+import org.springframework.stereotype.Service;
+
+/**
+ * 角色业务处理层
+ *
+ * @author QH
+ */
+@Service
+public class SysRoleServiceImpl implements ISysRoleService {
+}
